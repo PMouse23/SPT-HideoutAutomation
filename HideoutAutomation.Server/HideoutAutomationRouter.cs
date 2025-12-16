f32af73484348b6d4a031740fb1436e43283e9fb
KnotScripts <Mouse2023Hub@gmail.com>
2025-12-16 22:39:31 +0100

    added slashes

--- HideoutAutomation.Server/HideoutAutomationRouter.cs
+++ HideoutAutomation.Server/HideoutAutomationRouter.cs
@@ -7,9 +7,9 @@ namespace HideoutAutomation.Server
 {
     [Injectable]
     public class HideoutAutomationRouter(JsonUtil jsonUtil, HideoutAutomationService automationService) : StaticRouter(jsonUtil, [
-        new RouteAction<ProductionCountRequestData>("hideoutautomation/ProductionCount",
+        new RouteAction<ProductionCountRequestData>("/hideoutautomation/ProductionCount",
                  async (url, requestData, sessionId, output) => jsonUtil.Serialize(await automationService.ProductionCount(sessionId, requestData))!),
-        new RouteAction<ProductionPreservationRequestData>("hideoutautomation/preservation",
+        new RouteAction<ProductionPreservationRequestData>("/hideoutautomation/preservation",
                 async (url, requestData, sessionId, output) => jsonUtil.Serialize(await automationService.MakePreservation(sessionId, requestData))!)
         ])
     {
