3511a3a72627d3aa66e165753896b7163617ec3e
KnotScripts <Mouse2023Hub@gmail.com>
2025-12-23 19:56:54 +0100

    Removed unused functions and services.

--- HideoutAutomation.Server/HideoutAutomationService.cs
+++ HideoutAutomation.Server/HideoutAutomationService.cs
@@ -7,10 +7,7 @@ using SPTarkov.Server.Core.Models.Common;
 using SPTarkov.Server.Core.Models.Eft.Common;
 using SPTarkov.Server.Core.Models.Eft.Common.Tables;
 using SPTarkov.Server.Core.Models.Eft.Hideout;
-using SPTarkov.Server.Core.Models.Eft.ItemEvent;
 using SPTarkov.Server.Core.Models.Enums.Hideout;
-using SPTarkov.Server.Core.Models.Utils;
-using SPTarkov.Server.Core.Routers;
 using SPTarkov.Server.Core.Services;
 using System;
 using System.Collections.Generic;
@@ -21,97 +18,15 @@ namespace HideoutAutomation.Server
 {
     [Injectable(InjectionType = InjectionType.Singleton)]
     public class HideoutAutomationService(
-        ISptLogger<HideoutAutomationService> logger,
         ProfileHelper profileHelper,
         HideoutAutomationStore hideoutAutomationStore,
         DatabaseService databaseService,
-        HideoutController hideoutController,
-        InventoryHelper inventoryHelper,
-        EventOutputHolder eventOutputHolder
+        HideoutController hideoutController
         )
     {
-        public ValueTask<int> AreaCount(MongoId sessionId, ProductionCountRequestData requestData)
-        {
-            HideoutAutomationData? data = this.GetHideoutAutomationData(sessionId);
-            if (data == null)
-                return ValueTask.FromResult(0);
-            int count = 0;
-            HideoutAreas area = requestData.Area;
-            if (data.AreaProductions.ContainsKey(area))
-                count = data.AreaProductions[area].Count;
-            return ValueTask.FromResult(count);
-        }
-
         public ValueTask<HideoutProduction?> GetHideoutProduction(MongoId recipeId)
         {
             return ValueTask.FromResult(this.getHideoutProduction(recipeId));
-        }
-
-        public void Log(string data)
-        {
-            logger.Success(data);
-        }
-
-        public ValueTask<bool> MakePreservation(MongoId sessionId, ProductionPreservationRequestData requestData)
-        {
-            logger.Success($"HideoutAutomation: MakePreservation {sessionId}");
-
-            PmcData? pmcData = profileHelper.GetPmcProfile(sessionId);
-            if (pmcData == null)
-                return ValueTask.FromResult(false);
-            if (pmcData.Id is not MongoId profileId)
-                return ValueTask.FromResult(false);
-
-            HideoutAutomationData data = hideoutAutomationStore.Get(profileId);
-            List<Item>? items = pmcData.Inventory?.Items;
-            if (items == null)
-                return ValueTask.FromResult(false);
-
-            HideoutAreas area = requestData.Area;
-            RecipePreservation recipePreservation = new RecipePreservation()
-            {
-                RecipeId = requestData.RecipeId
-            };
-            foreach (IdWithCount idWithCount in requestData.Items)
-            {
-                MongoId id = idWithCount.Id;
-                Item? item = items.Find(i => i.Id == id);
-                if (item == null)
-                {
-                    logger.Error($"HideoutAutomation: Cannot find item {id}");
-                    return ValueTask.FromResult(false);
-                }
-                if (idWithCount.Count is double count)
-                {
-                    recipePreservation.ItemPreservations.Add(new ItemPreservation() { TemplateId = id, Count = count });
-                    ItemEventRouterResponse output = eventOutputHolder.GetOutput(sessionId);
-                    inventoryHelper.RemoveItemByCount(pmcData, id, (int)count, sessionId, output);
-                }
-            }
-            if (data.AreaRecipePreservations.ContainsKey(area) == false)
-                data.AreaRecipePreservations.Add(area, [recipePreservation]);
-            else
-                data.AreaRecipePreservations[area].Add(recipePreservation);
-            hideoutAutomationStore.Set(profileId);
-            logger.Success($"HideoutAutomation: Made preservation for recipe {recipePreservation.RecipeId}");
-            return ValueTask.FromResult(true);
-        }
-
-        public void ProduceNext(MongoId sessionId, PmcData pmcData, HideoutTakeProductionRequestData requestData)
-        {
-            if (pmcData.Id is not MongoId profileId)
-                return;
-            HideoutAutomationData? data = this.GetHideoutAutomationData(sessionId);
-            if (data == null)
-                return;
-            HideoutAreas? area = this.getHideoutArea(requestData.RecipeId);
-            if (area == null)
-                return;
-            if (data.AreaProductions.TryGetValue(area.Value, out Stack<HideoutSingleProductionStartRequestData>? values) == false)
-                return;
-            if (values.Count == 0)
-                return;
-            this.ProduceNext(sessionId, pmcData, profileId, values);
         }
 
         public MongoId? ProduceNext(MongoId sessionId, PmcData pmcData, HideoutAreas area)
