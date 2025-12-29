6066005ebfb4ec8847ad5ed12d7187c2c79f400e
KnotScripts <Mouse2023Hub@gmail.com>
2025-12-29 21:21:53 +0100

    Created RaidHelper to check if HasRaidLoaded.

---
+++ Helpers/RaidHelper.cs
@@ -0,0 +1,15 @@
+﻿using Comfort.Common;
+using EFT;
+using SPT.SinglePlayer.Utils.InRaid;
+
+namespace HideoutAutomation.Helpers
+{
+    internal static class RaidHelper
+    {
+        internal static bool HasRaidLoaded()
+        {
+            return RaidTimeUtil.HasRaidLoaded()
+                && Singleton<AbstractGame>.Instance?.GameType != EGameType.Hideout;
+        }
+    }
+}
